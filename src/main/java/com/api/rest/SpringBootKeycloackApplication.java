201cf582105be8a9fd885949c4bcd751b8111006
maen210212 <maen210212@gmail.com>
2024-03-25 19:23:38 -0600

    Configuration of Spring Security.

--- src/main/java/com/api/rest/SpringBootKeycloackApplication.java
+++ src/main/java/com/api/rest/SpringBootKeycloackApplication.java
@@ -9,5 +9,4 @@ public class SpringBootKeycloackApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootKeycloackApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/api/rest/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.api.rest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    /**
+     * Metodo para hacer la configuracion de la aplicacion.
+     */
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(http -> http.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth -> {
+                    oauth.jwt(jwt -> {});
+                })
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) )
+                        .build();
+    }
+}
